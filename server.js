9f5822d5e03b013f379add86480c41805407b974
Valentín Blanco <valentin_gomez1@hotmail.com>
2016-08-24 12:00:38 +0000

    Finished server.js; added multer package

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 var express = require("express");
 var app = express();
+var multer = require("multer");
+var upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 20971520
+    }
+});
+
+function endJSON(json, res) {
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(json));
+}
+
+app.post("/get_file_size", upload.single("file"),
+    function(req, res, next) {
+        if (req.file) {
+            endJSON({
+                size: req.file.size
+            }, res);
+        
+            req.file.buffer = null;
+        } else {
+            endJSON({
+                error: "No file submitted"
+            }, res);
+        }
+    });
 
 app.use("/", express.static("public"));
+
+app.use(
+    function(err, req, res, next) {
+        console.error(err);
+            
+        endJSON({
+            error: err.message
+        }, res);
+    });
 
 var port = process.env.PORT || 8080;
 app.listen(port,
